7fa3b072e426a239bf6a1dfc4424ffdf4da1a00a
Robin Buschmann <cm@Robins-MBP.fritz.box>
2016-06-20 08:41:21 +0200

    generic type added for better type support

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,11 @@
 declare namespace angular {
 
-  interface Type extends Function {
-  }
-
   interface IPromise<T> {
 
     /**
      * Shorthand for promise.then(null, errorCallback) with possibility to filter by error type
      */
-    catch<TResult>(errorType: Type, onRejected: (reason: any) => IPromise<TResult>|TResult): IPromise<TResult>;
+    catch<TResult, TError extends Error|Error>(errorType: {new (...arg): TError}, onRejected: (reason: TError) => IPromise<TResult>|TResult): IPromise<TResult>;
 
   }
 }
